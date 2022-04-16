527908df283a308d9e79bb427f4155ba6d5bba96
Harshit Raj <harshitr20@iitk.ac.in>
2022-04-16 15:51:52 +0530

    Setup cors

--- go.mod
+++ go.mod
@@ -4,13 +4,15 @@ go 1.17
 
 require (
 	github.com/99designs/gqlgen v0.17.2
+	github.com/go-chi/chi v1.5.4
+	github.com/gorilla/websocket v1.4.2
+	github.com/rs/cors v1.8.2
 	github.com/vektah/gqlparser/v2 v2.4.0
 )
 
 require (
 	github.com/agnivade/levenshtein v1.1.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.0 // indirect
 	github.com/matryer/moq v0.2.3 // indirect
 	github.com/mitchellh/mapstructure v1.2.3 // indirect
--- server.go
+++ server.go
@@ -6,7 +6,13 @@ import (
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+
+	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
+
 	"github.com/spo-iitk/ras-backend/graph"
 	"github.com/spo-iitk/ras-backend/graph/generated"
 )
@@ -19,7 +25,25 @@ func main() {
 		port = defaultPort
 	}
 
+	router := chi.NewRouter()
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	srv.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return r.Host == "http://localhost:3000"
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	})
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
