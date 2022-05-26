dc166fca7f977047bd34a556b246e07611841d07
Abhishek Shree <abhi1234456@gmail.com>
2022-05-27 01:34:11 +0530

    Add mass mail

--- config/config.go
+++ config/config.go
@@ -10,11 +10,12 @@ const (
 )
 
 var (
-	EmailUser = os.Getenv("EMAIL_USER")
-	EmailPass = os.Getenv("EMAIL_PASS")
-	EmailHost = os.Getenv("EMAIL_HOST")
-	EmailPort = os.Getenv("EMAIL_PORT")
-	EmailType = os.Getenv("EMAIL_TYPE") // IITK or Personal
+	EmailUser    = os.Getenv("EMAIL_USER")
+	EmailPass    = os.Getenv("EMAIL_PASS")
+	EmailHost    = os.Getenv("EMAIL_HOST")
+	EmailPort    = os.Getenv("EMAIL_PORT")
+	EmailType    = os.Getenv("EMAIL_TYPE") // IITK or Personal
+	EmailWebteam = "spowebteam@gmail.com"
 )
 
 func CfgInit() {}
--- mail/service.go
+++ mail/service.go
@@ -1,12 +1,23 @@
 package mail
 
 import (
+	"fmt"
 	"log"
 	"net/smtp"
+	"strings"
 
 	"github.com/spo-iitk/ras-backend/config"
 	"github.com/spo-iitk/ras-backend/models"
 )
+
+type Mail struct {
+	Sender  string
+	To      []string
+	Cc      []string
+	Bcc     []string
+	Subject string
+	Body    string
+}
 
 type StudentMailingEntity struct {
 	Student models.Student
@@ -18,9 +29,35 @@ type CompanyMailingEntity struct {
 	Msg     []byte
 }
 
-type Mail interface {
+type MassMailEntity struct {
+	// From the docs Sending "Bcc" messages is accomplished by including an email address in the to parameter but not including it in the msg headers.
+	to  []string //NOTE: list of emails seperated by comma
+	Msg []byte
+}
+
+type Mailer interface {
+	MassMailerService(mail_channel chan MassMailEntity)
 	StudentMailerService(student_mail_channel chan models.Student)
 	CompanyMailerService(company_mail_channel chan models.CompanyHR)
+}
+
+func (mail *Mail) BuildMessage() []byte {
+	header := ""
+	header += fmt.Sprintf("From: %s\r\n", mail.Sender)
+	if len(mail.To) > 0 {
+		header += fmt.Sprintf("To: %s\r\n", strings.Join(mail.To, ";"))
+	}
+	if len(mail.Cc) > 0 {
+		header += fmt.Sprintf("Cc: %s\r\n", strings.Join(mail.Cc, ";"))
+	}
+	if len(mail.Bcc) > 0 {
+		header += fmt.Sprintf("Bcc: %s\r\n", strings.Join(mail.Bcc, ";"))
+	}
+
+	header += fmt.Sprintf("Subject: %s\r\n", mail.Subject)
+	header += "\r\n" + mail.Body
+
+	return []byte(header)
 }
 
 func StudentMailerService(student_mail_channel chan StudentMailingEntity) {
@@ -69,6 +106,20 @@ func CompanyMailerService(company_mail_channel chan CompanyMailingEntity) {
 			mailCounter += 1
 			log.Printf("Mailed %v\n", u.Company.ID)
 			log.Printf("Mails sent since inception: %v\n", mailCounter)
+		}
+	}
+}
+
+func MassMailerService(mail_channel chan MassMailEntity) {
+	auth := smtp.PlainAuth("", config.EmailUser, config.EmailPass, config.EmailHost)
+	to := []string{config.EmailWebteam}
+	for u := range mail_channel {
+		err := smtp.SendMail(config.EmailHost+":"+config.EmailPort, auth, config.EmailUser, to, u.Msg)
+		if err != nil {
+			log.Printf("ERROR: while mailing users %v\n", u.to)
+			log.Println(err)
+		} else {
+			log.Printf("Mailed %v\n", u.to)
 		}
 	}
 }
