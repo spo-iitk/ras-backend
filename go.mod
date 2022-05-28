9ed44844f009b6d336694ae152370ef95fdffa74
Abhishek Shree <abhi1234456@gmail.com>
2022-05-28 21:48:43 +0530

    changes to mail, add config

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/viper v1.12.0
-	gorm.io/driver/postgres v1.3.6
+	gorm.io/driver/postgres v1.3.7
 	gorm.io/gorm v1.23.5
 )
 
--- mail/service.go
+++ mail/service.go
@@ -2,12 +2,12 @@ package mail
 
 import (
 	"fmt"
-	"log"
 	"net/smtp"
 	"strings"
 
-	"github.com/spo-iitk/ras-backend/config"
-	"github.com/spo-iitk/ras-backend/models"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/spf13/viper"
 )
 
 type Mail struct {
@@ -19,26 +19,10 @@ type Mail struct {
 	Body    string
 }
 
-type StudentMailingEntity struct {
-	Student models.Student
-	Msg     []byte
-}
-
-type CompanyMailingEntity struct {
-	Company models.CompanyHR
-	Msg     []byte
-}
-
-type MassMailEntity struct {
+type MailEntity struct {
 	// From the docs Sending "Bcc" messages is accomplished by including an email address in the to parameter but not including it in the msg headers.
 	to  []string //NOTE: list of emails seperated by comma
 	Msg []byte
-}
-
-type Mailer interface {
-	MassMailerService(mail_channel chan MassMailEntity)
-	StudentMailerService(student_mail_channel chan models.Student)
-	CompanyMailerService(company_mail_channel chan models.CompanyHR)
 }
 
 func (mail *Mail) BuildMessage() []byte {
@@ -54,67 +38,26 @@ func (mail *Mail) BuildMessage() []byte {
 		header += fmt.Sprintf("Bcc: %s\r\n", strings.Join(mail.Bcc, ";"))
 	}
 
-	header += fmt.Sprintf("Subject: %s\r\n", mail.Subject)
+	header += fmt.Sprintf("Subject: %s | Recruitment Automation Portal\r\n", mail.Subject)
 	header += "\r\n" + mail.Body
+
+	header += "\r\n\r\n This is an auto-generated email. Please do not reply."
 
 	return []byte(header)
 }
 
-func StudentMailerService(student_mail_channel chan StudentMailingEntity) {
-	mailCounter := 0
-	auth := smtp.PlainAuth("", config.EmailUser, config.EmailPass, config.EmailHost)
+func MailerService(mail_channel chan MailEntity) {
+	user := viper.GetString("MAIL.USER")
+	pass := viper.GetString("MAIL.PASS")
+	host := viper.GetString("MAIL.HOST")
+	port := viper.GetString("MAIL.PORT")
+	address := fmt.Sprintf("%s:%s", host, port)
 
-	for u := range student_mail_channel {
-		log.Println("Setting up smtp")
+	auth := smtp.PlainAuth("", user, pass, host)
+	to := []string{viper.GetString("MAIL.WEBTEAM")}
 
-		var to []string
-
-		if config.EmailType == config.IITKMail {
-			to = []string{u.Student.IITKEmail}
-		} else {
-			to = []string{u.Student.PersonalEmail}
-		}
-
-		err := smtp.SendMail(config.EmailHost+":"+config.EmailPort, auth, config.EmailUser, to, u.Msg)
-
-		if err != nil {
-			log.Printf("ERROR: while mailing user %v, ID %v\n", to, u.Student.ID)
-			log.Println(err)
-		} else {
-			mailCounter += 1
-			log.Printf("Mailed %v\n", u.Student.ID)
-			log.Printf("Mails sent since inception: %v\n", mailCounter)
-		}
-	}
-}
-
-func CompanyMailerService(company_mail_channel chan CompanyMailingEntity) {
-	mailCounter := 0
-	auth := smtp.PlainAuth("", config.EmailUser, config.EmailPass, config.EmailHost)
-
-	for u := range company_mail_channel {
-		log.Println("Setting up smtp")
-
-		to := []string{u.Company.Email}
-
-		err := smtp.SendMail(config.EmailHost+":"+config.EmailPort, auth, config.EmailUser, to, u.Msg)
-
-		if err != nil {
-			log.Printf("ERROR: while mailing user %v, ID %v\n", to, u.Company.ID)
-			log.Println(err)
-		} else {
-			mailCounter += 1
-			log.Printf("Mailed %v\n", u.Company.ID)
-			log.Printf("Mails sent since inception: %v\n", mailCounter)
-		}
-	}
-}
-
-func MassMailerService(mail_channel chan MassMailEntity) {
-	auth := smtp.PlainAuth("", config.EmailUser, config.EmailPass, config.EmailHost)
-	to := []string{config.EmailWebteam}
 	for u := range mail_channel {
-		err := smtp.SendMail(config.EmailHost+":"+config.EmailPort, auth, config.EmailUser, to, u.Msg)
+		err := smtp.SendMail(address, auth, user, to, u.Msg)
 		if err != nil {
 			log.Printf("ERROR: while mailing users %v\n", u.to)
 			log.Println(err)
