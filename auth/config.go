32656b9a2dc87fa6d3e58a4eba7d050d37321a8c
Harshit Raj <harshitr20@iitk.ac.in>
2022-05-30 18:20:04 +0530

    Update config files

--- auth/config.go
+++ auth/config.go
@@ -3,13 +3,14 @@ package auth
 import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	_ "github.com/spo-iitk/ras-backend/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var db *gorm.DB
 
-func OpenConnection() {
+func openConnection() {
 	host := viper.GetString("DATABASE.HOST")
 	port := viper.GetString("DATABASE.PORT")
 	password := viper.GetString("DATABASE.PASSWORD")
@@ -19,6 +20,7 @@ func OpenConnection() {
 
 	dsn := "host=" + host + " user=" + user + " password=" + password
 	dsn += " dbname=" + dbName + " port=" + port + " sslmode=disable TimeZone=Asia/Kolkata"
+
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to auth database: ", err)
@@ -26,7 +28,17 @@ func OpenConnection() {
 	}
 
 	db = database
-	db.AutoMigrate(&User{}, &OTP{})
+
+	err = db.AutoMigrate(&User{}, &OTP{})
+	if err != nil {
+		log.Fatal("Failed to migrate auth database: ", err)
+		panic(err)
+	}
 
 	log.Info("Connected to auth database")
+}
+
+func init() {
+	openConnection()
+	go cleanupOTP()
 }
