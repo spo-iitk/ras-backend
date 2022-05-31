0c243d816c42f75a1b413443878ebffb010af136
somyagupta18 <tosomyagupta@gmail.com>
2022-05-31 15:32:15 +0530

    Use middleware

--- cmd/ras.go
+++ cmd/ras.go
@@ -4,17 +4,19 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spo-iitk/ras-backend/middleware"
 	"github.com/spo-iitk/ras-backend/router"
 )
 
 func rasServer() *http.Server {
 	PORT := "8080"
-	r := gin.New()
-	router.RASRouter(r)
+	engine := gin.New()
+	engine.Use(middleware.Authenticator())
+	router.RASRouter(engine)
 
 	server := &http.Server{
 		Addr:         ":" + PORT,
-		Handler:      r,
+		Handler:      engine,
 		ReadTimeout:  readTimeout,
 		WriteTimeout: writeTimeout,
 	}
