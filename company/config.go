3235bd2317ad3c17ac3b64120d5373f9c27b924a
Harshit Raj <harshitr20@iitk.ac.in>
2022-06-01 20:02:11 +0530

    Add config files

---
+++ company/config.go
@@ -0,0 +1,43 @@
+package company
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func openConnection() {
+	host := viper.GetString("DATABASE.HOST")
+	port := viper.GetString("DATABASE.PORT")
+	password := viper.GetString("DATABASE.PASSWORD")
+
+	dbName := viper.GetString("COMPANY.DBNAME")
+	user := dbName + viper.GetString("DATABASE.USER")
+
+	dsn := "host=" + host + " user=" + user + " password=" + password
+	dsn += " dbname=" + dbName + " port=" + port + " sslmode=disable TimeZone=Asia/Kolkata"
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		logrus.Fatal("Failed to connect to company database: ", err)
+		panic(err)
+	}
+
+	db = database
+
+	//! TODO : Fix this
+	err = db.AutoMigrate()
+	if err != nil {
+		logrus.Fatal("Failed to migrate company database: ", err)
+		panic(err)
+	}
+
+	logrus.Info("Connected to company database")
+}
+
+func init() {
+	openConnection()
+}
---
+++ cycle/config.go
@@ -0,0 +1,43 @@
+package cycle // will be reanamed later
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func openConnection() {
+	host := viper.GetString("DATABASE.HOST")
+	port := viper.GetString("DATABASE.PORT")
+	password := viper.GetString("DATABASE.PASSWORD")
+
+	dbName := viper.GetString("CYCLE.DBNAME")
+	user := dbName + viper.GetString("DATABASE.USER")
+
+	dsn := "host=" + host + " user=" + user + " password=" + password
+	dsn += " dbname=" + dbName + " port=" + port + " sslmode=disable TimeZone=Asia/Kolkata"
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		logrus.Fatal("Failed to connect to cycle database: ", err)
+		panic(err)
+	}
+
+	db = database
+
+	//! TODO : Fix this
+	err = db.AutoMigrate()
+	if err != nil {
+		logrus.Fatal("Failed to migrate cycle database: ", err)
+		panic(err)
+	}
+
+	logrus.Info("Connected to cycle database")
+}
+
+func init() {
+	openConnection()
+}
