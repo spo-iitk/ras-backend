6ebcdbd55ea7c651d60b57ca312a5865d41ef553
somyagupta18 <tosomyagupta@gmail.com>
2022-06-01 13:57:27 +0530

    Add student to cmd

---
+++ cmd/student.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"github.com/spo-iitk/ras-backend/middleware"
+	"github.com/spo-iitk/ras-backend/student"
+)
+
+func studentServer() *http.Server {
+	PORT := viper.GetString("STUDENT.PORT")
+	engine := gin.New()
+	engine.Use(middleware.Authenticator())
+	student.Router(engine)
+
+	server := &http.Server{
+		Addr:         ":" + PORT,
+		Handler:      engine,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+	}
+
+	return server
+}
