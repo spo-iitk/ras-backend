2dc1fb8cee0cb70082117c66603fa7199deea245
Harshit Raj <harshitr20@iitk.ac.in>
2022-06-05 17:28:12 +0530

    Add mails to cmd

--- auth/router.go
+++ auth/router.go
@@ -11,8 +11,8 @@ func Router(mail_channel chan mail.Mail, r *gin.Engine) {
 	{
 		auth.POST("/login", loginHandler)
 		auth.POST("/signup", signUpHandler(mail_channel))
-		auth.POST("/otp", otpHandler)
-		auth.POST("/reset-password", resetPasswordHandler)
+		auth.POST("/otp", otpHandler(mail_channel))
+		auth.POST("/reset-password", resetPasswordHandler(mail_channel))
 		auth.POST("/company-signup", companySignUpHandler(mail_channel))
 		auth.GET("/whoami", ras.PlaceHolderController) // who am i, if not exploited
 	}
