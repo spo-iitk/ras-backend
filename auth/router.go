8c4cc2f5449320486792f8ead2a3f0f3278eb882
Abhishek Shree <abhi1234456@gmail.com>
2022-06-07 02:04:38 +0530

    fix auth router

--- auth/router.go
+++ auth/router.go
@@ -12,7 +12,7 @@ func Router(mail_channel chan mail.Mail, r *gin.Engine) {
 		auth.POST("/signup", signUpHandler(mail_channel))
 		auth.POST("/otp", otpHandler(mail_channel))
 		auth.POST("/reset-password", resetPasswordHandler(mail_channel))
-		auth.POST("/company-signup", companySignUpHandler(mail_channel))
+		auth.POST("/company-signup", CompanySignUpHandler(mail_channel))
 		auth.GET("/whoami", whoamiHandler) // who am i, if not exploited
 	}
 }
