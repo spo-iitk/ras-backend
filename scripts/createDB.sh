920a2a13e9b5edeac1ec55586c6f5f0b0cb79b8c
Harshit Raj <harshitr20@iitk.ac.in>
2022-06-06 12:09:41 +0530

    add docker-entrypoint-initdb.d

--- scripts/createDB.sh
+++ scripts/createDB.sh
@@ -2,10 +2,13 @@
 
 CreateDB() {
    echo "Creating DB $1"
-   sudo -u postgres psql -c "CREATE ROLE $1admin WITH PASSWORD 'b2Led2ke';"
+   sudo -u postgres psql -c "CREATE ROLE $1admin WITH LOGIN PASSWORD 'b2Led2ke';"
    sudo -u postgres psql -c "CREATE DATABASE $1;"
-   sudo -u postgres psql -c "ALTER ROLE $1admin WITH LOGIN;"
    sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $1 TO $1admin;"
+   # echo "CREATE ROLE $1admin WITH LOGIN PASSWORD 'b2Led2ke';" >> container/init.sql
+   # echo "CREATE DATABASE $1;" >> container/init.sql
+   # echo "GRANT ALL PRIVILEGES ON DATABASE $1 TO $1admin;" >> container/init.sql
+   # echo "" >> container/init.sql
 }
 
 sudo systemctl start postgresql
