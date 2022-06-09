20d692f4819821a3430424358779d9ef7abe3129
Harshit Raj <harshitr20@iitk.ac.in>
2022-06-09 23:41:24 +0530

    Restructure admin handlers

--- application/admin.events.go
+++ application/admin.events.go
@@ -4,8 +4,6 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spo-iitk/ras-backend/middleware"
-	"github.com/spo-iitk/ras-backend/rc"
 	"github.com/spo-iitk/ras-backend/util"
 )
 
@@ -28,8 +26,7 @@ func getAllEventsByRCHandler(ctx *gin.Context) {
 }
 
 func getEventsByPIDHandler(ctx *gin.Context) {
-	pid_string := ctx.Param("pid")
-	pid, err := util.ParseUint(pid_string)
+	pid, err := util.ParseUint(ctx.Param("pid"))
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -47,8 +44,7 @@ func getEventsByPIDHandler(ctx *gin.Context) {
 }
 
 func postEventHandler(ctx *gin.Context) {
-	pid_string := ctx.Param("pid")
-	pid, err := util.ParseUint(pid_string)
+	pid, err := util.ParseUint(ctx.Param("pid"))
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -109,35 +105,4 @@ func deleteEventHandler(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{"status": "Successfully deleted event"})
-}
-
-func getEventsByStudentHandler(ctx *gin.Context) {
-	rid_string := ctx.Param("rid")
-	rid, err := util.ParseUint(rid_string)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	user_email := middleware.GetUserID(ctx)
-	if user_email == "" {
-		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
-		return
-	}
-
-	sid, err := rc.FetchStudentRCIDs(ctx, rid, []string{user_email})
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	events := []ProformaEvent{}
-	err = fetchEventsByStudent(ctx, sid[0], &events)
-
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	ctx.JSON(http.StatusOK, events)
 }
