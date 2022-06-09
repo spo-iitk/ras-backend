5ade5ec54705773db031d09f1314df776ab98bff
Harshit Raj <harshitr20@iitk.ac.in>
2022-06-09 23:31:34 +0530

    Update db structure

---
+++ application/db.student.go
@@ -0,0 +1,34 @@
+package application
+
+import "github.com/gin-gonic/gin"
+
+// func createStudentEvent(ctx *gin.Context, eventStudent *EventStudent) error {
+// 	tx := db.WithContext(ctx).Create(eventStudent)
+// 	return tx.Error
+// }
+
+func createStudentEvents(ctx *gin.Context, eventStudents *[]EventStudent) error {
+	tx := db.WithContext(ctx).Create(eventStudents)
+	return tx.Error
+}
+
+func getRecruitmentStats(ctx *gin.Context, rid uint, stats *[]EventStudent) error {
+	tx := db.WithContext(ctx).Joins("proforma_event", db.Where("name IN", []EventType{Recruited, PIOPPOACCEPTED})).Where("recruitment_cycle_id = ?", rid).Find(stats)
+	return tx.Error
+}
+
+func fetchStudentRCIDByEvents(ctx *gin.Context, eventID uint) ([]uint, error) {
+	var ids []uint
+	tx := db.WithContext(ctx).Model(&EventStudent{}).Where("proforma_event_id = ?", eventID).Pluck("student_recruitment_cycle_id", &ids)
+	return ids, tx.Error
+}
+
+func fetchStudentsByEvent(ctx *gin.Context, eventID uint, students *[]EventStudent) error {
+	tx := db.WithContext(ctx).Where("proforma_event_id = ?", eventID).Find(students)
+	return tx.Error
+}
+
+func createEventStudents(ctx *gin.Context, eventStudents *[]EventStudent) error {
+	tx := db.WithContext(ctx).FirstOrCreate(eventStudents)
+	return tx.Error
+}
