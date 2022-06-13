17a81e6c4e46f2e181fe0c1ca45c08223382e3ca
Harshit Raj <harshitr20@iitk.ac.in>
2022-06-13 16:25:14 +0530

    Add fetchApplicationCount

--- application/db.application.go
+++ application/db.application.go
@@ -12,3 +12,9 @@ func deleteApplication(ctx *gin.Context, pid uint, sid uint) error {
 	tx := db.WithContext(ctx).Where("proforma_event_id = ? AND student_recruitment_cycle_id = ?", pid, sid).Delete(&EventStudent{})
 	return tx.Error
 }
+
+func fetchApplicationCount(ctx *gin.Context, sid uint) (bool, error) {
+	var count int64
+	tx := db.WithContext(ctx).Model(&EventStudent{}).Distinct().Where("student_recruitment_cycle_id = ?", sid).Count(&count)
+	return false, tx.Error
+}
