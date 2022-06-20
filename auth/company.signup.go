85bf0b0f5ad6fbb73d54bf73b42dbded36215e92
Abhishek Shree <abhi1234456@gmail.com>
2022-06-20 16:04:41 +0530

    Mail SPO on company signup request

--- auth/company.signup.go
+++ auth/company.signup.go
@@ -29,6 +29,15 @@ func companySignUpHandler(mail_channel chan mail.Mail) gin.HandlerFunc {
 			"Registration requested on RAS",
 			"Dear "+signupReq.CompanyName+",\n\nYou have been requested to be registered on RAS. We will get back to you soon. For any queries, please contact us at spo@iitk.ac.in")
 
+		mail_channel <- mail.GenerateMail("spo@iitk.ac.in",
+			"Registration requested on RAS",
+			"Company "+signupReq.CompanyName+" has requested to be registered on RAS. The details are as follows:\n\n"+
+				"Name: "+signupReq.Name+"\n"+
+				"Designation: "+signupReq.Designation+"\n"+
+				"Email: "+signupReq.Email+"\n"+
+				"Phone: "+signupReq.Phone+"\n"+
+				"Comments: "+signupReq.Comments+"\n")
+
 		ctx.JSON(http.StatusOK, gin.H{"status": "Successfully Requested"})
 	}
 }
