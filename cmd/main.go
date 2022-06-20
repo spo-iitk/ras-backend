ca39ccd270e1efbe5b15aa8a59c1190c6022c457
Harshit Raj <harshitr20@iitk.ac.in>
2022-06-20 22:14:03 +0530

    Add release mode

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/spo-iitk/ras-backend/config"
 	"github.com/spo-iitk/ras-backend/mail"
 	"golang.org/x/sync/errgroup"
@@ -19,6 +20,8 @@ func main() {
 	mail_channel := make(chan mail.Mail)
 
 	go mail.Service(mail_channel)
+
+	gin.SetMode(gin.ReleaseMode)
 
 	g.Go(func() error {
 		return authServer(mail_channel).ListenAndServe()
