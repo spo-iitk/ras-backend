0e2b5685db41bf7f24ba79420e3da8580f2ecb9b
Harshit Raj <harshitr20@iitk.ac.in>
2022-06-25 16:15:44 +0530

    add debug

--- rc/db.question.go
+++ rc/db.question.go
@@ -23,7 +23,7 @@ func deleteStudentQuestion(ctx *gin.Context, qid string) error {
 }
 
 func fetchStudentQuestionsAnswers(ctx *gin.Context, rid, sid uint, questions *[]getStudentEnrollmentResponse) error {
-	tx := db.WithContext(ctx).Model(&RecruitmentCycleQuestion{}).
+	tx := db.WithContext(ctx).Debug().Model(&RecruitmentCycleQuestion{}).
 		Joins("LEFT JOIN recruitment_cycle_questions_answers ON recruitment_cycle_questions_answers.recruitment_cycle_question_id = recruitment_cycle_questions.id").
 		Select("recruitment_cycle_questions.*, recruitment_cycle_questions_answers.answer").
 		Where(
