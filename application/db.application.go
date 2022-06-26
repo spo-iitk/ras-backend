76b54f676569672ed4945aaeeb2ddde92077763a
Abhishek Shree <abhi1234456@gmail.com>
2022-06-26 20:49:25 +0530

    Update db call

--- application/db.application.go
+++ application/db.application.go
@@ -93,10 +93,9 @@ func createApplication(ctx *gin.Context, application *EventStudent, answers *[]A
 func fetchApplicantDetails(ctx *gin.Context, pid uint, students *[]ApplicantsByRole) error {
 	tx := db.WithContext(ctx).Model(&EventStudent{}).
 		Joins("JOIN proforma_events ON proforma_events.id = event_students.proforma_event_id").
-		Joins("JOIN application_resumes ON application_resumes.proforma_id = proforma_events.id").
+		Joins("JOIN application_resumes ON application_resumes.proforma_id = proforma_events.proforma_id").
 		Where("proforma_events.proforma_id = ? ", pid).
 		Select("event_students.student_recruitment_cycle_id as student_id, application_resumes.resume as resume_link, proforma_events.sequence as status").
-		// Group("event_students.student_recruitment_cycle_id").
 		Scan(students)
 
 	return tx.Error
