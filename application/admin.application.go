0e59cd12e202277632a9c40ec0ba71eb3d37c139
Abhishek Shree <abhi1234456@gmail.com>
2022-06-27 00:49:24 +0530

    WIP (2)

--- application/admin.application.go
+++ application/admin.application.go
@@ -61,6 +61,11 @@ func getStudentsByRole(ctx *gin.Context) {
 	var allStudents []rc.StudentRecruitmentCycle
 	rc.FetchStudentBySRID(ctx, srids, &allStudents)
 
+	var allStudentsMap = make(map[uint]*rc.StudentRecruitmentCycle)
+	for i := range allStudents {
+		allStudentsMap[allStudents[i].ID] = &allStudents[i]
+	}
+
 	var sid []uint
 	for _, student := range allStudents {
 		sid = append(sid, student.StudentID)
@@ -69,9 +74,49 @@ func getStudentsByRole(ctx *gin.Context) {
 	var allStudentDetails []student.Student
 	student.FetchStudentsByID(ctx, sid, &allStudentDetails)
 
+	var allStudentDetailsMap = make(map[uint]*student.Student)
+	for i := range allStudentDetails {
+		allStudentDetailsMap[allStudentDetails[i].ID] = &allStudentDetails[i]
+	}
+
 	var validApplicants []studentAdminsideResponse
-	// for _, student := range applied {
-	// }
+	for _, student := range applied {
+		if allStudentsMap[student.StudentRCID].IsFrozen {
+			continue
+		}
+
+		applicant_details := studentAdminsideResponse{}
+		applicant_details.ID = student.StudentRCID
+		applicant_details.Resume = student.ResumeLink
+		applicant_details.Status = student.Status
+
+		sid := allStudentsMap[student.StudentRCID].StudentID
+
+		applicant_details.CPI = allStudentsMap[student.StudentRCID].CPI
+
+		applicant_details.Name = allStudentDetailsMap[sid].Name
+		applicant_details.Email = allStudentDetailsMap[sid].IITKEmail
+		applicant_details.ProgramDepartmentID = allStudentDetailsMap[sid].ProgramDepartmentID
+		applicant_details.SecondaryProgramDepartmentID = allStudentDetailsMap[sid].SecondaryProgramDepartmentID
+		applicant_details.CurrentCPI = allStudentDetailsMap[sid].CurrentCPI
+		applicant_details.UGCPI = allStudentDetailsMap[sid].UGCPI
+		applicant_details.TenthBoard = allStudentDetailsMap[sid].TenthBoard
+		applicant_details.TenthYear = allStudentDetailsMap[sid].TenthYear
+		applicant_details.TenthMarks = allStudentDetailsMap[sid].TenthMarks
+		applicant_details.TwelfthBoard = allStudentDetailsMap[sid].TwelfthBoard
+		applicant_details.TwelfthYear = allStudentDetailsMap[sid].TwelfthYear
+		applicant_details.TwelfthMarks = allStudentDetailsMap[sid].TwelfthMarks
+		applicant_details.EntranceExam = allStudentDetailsMap[sid].EntranceExam
+		applicant_details.EntranceExamRank = allStudentDetailsMap[sid].EntranceExamRank
+		applicant_details.Category = allStudentDetailsMap[sid].Category
+		applicant_details.CategoryRank = allStudentDetailsMap[sid].CategoryRank
+		applicant_details.CurrentAddress = allStudentDetailsMap[sid].CurrentAddress
+		applicant_details.PermanentAddress = allStudentDetailsMap[sid].PermanentAddress
+		applicant_details.FriendName = allStudentDetailsMap[sid].FriendName
+		applicant_details.FriendPhone = allStudentDetailsMap[sid].FriendPhone
+
+		validApplicants = append(validApplicants, applicant_details)
+	}
 
 	ctx.JSON(http.StatusOK, validApplicants)
 }
