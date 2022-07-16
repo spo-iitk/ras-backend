1a611808de2079b5c70082969f37e28bbf9049ef
Harshit Raj <75195728+1-Harshit@users.noreply.github.com>
2022-07-16 12:43:02 +0530

    Update model.go

--- application/model.go
+++ application/model.go
@@ -20,6 +20,7 @@ type Proforma struct {
 	HideDetails               bool         `json:"hide_details" gorm:"default:true"`
 	ActiveHR                  string       `json:"active_hr"`
 	Role                      string       `json:"role"`
+	Profile                   string       `json:"profile"`
 	TentativeJobLocation      string       `json:"tentative_job_location"`
 	JobDescription            string       `json:"job_description"`
 	CostToCompany             string       `json:"cost_to_company"`
