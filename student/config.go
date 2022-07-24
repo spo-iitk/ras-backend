240435cf706f7fbbf7886a7a5b6973787cb1892c
Harshit Raj <harshitr20@iitk.ac.in>
2022-07-24 16:27:05 +0530

    Add logers

--- student/config.go
+++ student/config.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/spo-iitk/ras-backend/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var db *gorm.DB
@@ -23,7 +24,7 @@ func openConnection() {
 	dsn += " dbname=" + dbName + " port=" + port + " sslmode=disable TimeZone=Asia/Kolkata"
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		// Logger: logger.Default.LogMode(logger.Info),
+		Logger: logger.Default.LogMode(logger.Info),
 	})
 	if err != nil {
 		logrus.Fatal("Failed to connect to student database: ", err)
