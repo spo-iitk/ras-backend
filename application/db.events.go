a85457fc4a314caa23bd27ccf6edc5f4f3c00699
Abhishek Shree <abhi1234456@gmail.com>
2022-08-25 12:29:18 +0530

    Fix typo

--- application/db.events.go
+++ application/db.events.go
@@ -10,7 +10,7 @@ func fetchEventsByRC(ctx *gin.Context, rid uint, events *[]getAllEventsByRCRespo
 		Joins("JOIN proformas ON proformas.id = proforma_events.proforma_id").
 		Where("proformas.deleted_at IS NULL AND proformas.recruitment_cycle_id = ?", rid).
 		Order("start_time DESC, proforma_id, sequence").
-		Select("proforma_events.*, proformas.company_name, proformas.role, proforma.profile").
+		Select("proforma_events.*, proformas.company_name, proformas.role, proformas.profile").
 		Find(events)
 	return tx.Error
 }
