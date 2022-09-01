601c4b149a4b565bfcce65f2ccaaaf8f09ce68eb
utkarsh-tf141 <utkarshm21@iitk.ac.in>
2022-09-02 00:35:19 +0530

    gcal events added

--- application/admin.events.go
+++ application/admin.events.go
@@ -1,13 +1,17 @@
 package application
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spo-iitk/ras-backend/rc"
 	"github.com/spo-iitk/ras-backend/util"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
 )
 
 type getAllEventsByRCResponse struct {
@@ -125,9 +129,42 @@ func putEventHandler(ctx *gin.Context) {
 			Tags:       fmt.Sprintf("scheduled,%s,%s,%s,%d", event.Name, proforma.Role, proforma.CompanyName, event.ID),
 			Attachment: "",
 		}, "Event Scheduled")
+		ctxb := context.Background()
+		srv, err := calendar.NewService(ctxb, option.WithCredentialsFile("../credentials.json"))
+		if err != nil {
+			log.Fatalf("Unable to retrieve Calendar client: %v", err)
+		}
+		cevent := &calendar.Event{
+			Summary:  event.Name,
+			Location: event.Venue,
+			Description: fmt.Sprintf(
+				"%s of profile %s - %s has been scheduled from %s to %s",
+				event.Name, proforma.Profile, proforma.CompanyName,
+				time.UnixMilli(int64(event.StartTime)).In(loc).Format("2006-01-02 15:04"),
+				time.UnixMilli(int64(event.EndTime)).In(loc).Format("2006-01-02 15:04")),
+			Start: &calendar.EventDateTime{
+				DateTime: fmt.Sprintf("%sT%s+05:30",
+					time.UnixMilli(int64(event.StartTime)).In(loc).Format("2006-01-02"),
+					time.UnixMilli(int64(event.StartTime)).In(loc).Format("15:04")),
+				TimeZone: "Asia/Kolkata",
+			},
+			End: &calendar.EventDateTime{
+				DateTime: fmt.Sprintf("%sT%s+05:30",
+					time.UnixMilli(int64(event.EndTime)).In(loc).Format("2006-01-02"),
+					time.UnixMilli(int64(event.EndTime)).In(loc).Format("15:04")),
+				TimeZone: "Asia/Kolkata",
+			},
+		}
+
+		calendarId := "767jlg7tf6bo62i7b1smr8rssc@group.calendar.google.com"
+		cevent, err = srv.Events.Insert(calendarId, cevent).Do()
+		if err != nil {
+			log.Fatalf("Unable to create event. %v\n", err)
+		}
+		fmt.Printf("Event created: %s\n", cevent.HtmlLink)
+		event.CalID = cevent.Id
 		return
 	}
-
 	ctx.JSON(http.StatusOK, event)
 }
 
@@ -137,12 +174,28 @@ func deleteEventHandler(ctx *gin.Context) {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	var cevent ProformaEvent
+	err = fetchEvent(ctx, eid, &cevent)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
 	err = deleteEvent(ctx, eid)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
+	ctxb := context.Background()
+	srv, err := calendar.NewService(ctxb, option.WithCredentialsFile("../credentials.json"))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Calendar client: %v", err)
+	}
+	calendarId := "767jlg7tf6bo62i7b1smr8rssc@group.calendar.google.com"
+	err = srv.Events.Delete(calendarId, cevent.CalID).Do()
+	if err != nil {
+		log.Fatalf("Unable to create event. %v\n", err)
+	}
 	ctx.JSON(http.StatusOK, gin.H{"status": "Successfully deleted event"})
 }
