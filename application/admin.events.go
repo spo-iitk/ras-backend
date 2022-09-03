d1ff0dc9709d9c9e0417b8cc4b996b1028dd4c79
utkarsh-tf141 <utkarshm21@iitk.ac.in>
2022-09-03 17:52:46 +0530

    again some minor changes

--- application/admin.events.go
+++ application/admin.events.go
@@ -147,20 +147,20 @@ func putEventHandler(ctx *gin.Context) {
 	}
 
 	cevent := &calendar.Event{
-		Summary:  fmt.Sprintf("%s of profile %s - %s", event.Name, proforma.Profile, proforma.CompanyName),
+		Summary:  fmt.Sprintf("%s - %s, %s", event.Name, proforma.Profile, proforma.CompanyName),
 		Location: event.Venue,
 		Description: fmt.Sprintf(
-			"%s of profile %s - %s has been scheduled from %s to %s\nhttps://placement.iitk.ac.in/student/rc/%d/event/%d",
+			"%s of profile %s - %s has been scheduled from %s to %s\nhttps://placement.iitk.ac.in/student/rc/%d/events/%d",
 			event.Name, proforma.Profile, proforma.CompanyName,
 			time.UnixMilli(int64(event.StartTime)).In(loc).Format("2006-01-02 15:04"),
 			time.UnixMilli(int64(event.EndTime)).In(loc).Format("2006-01-02 15:04"),
 			proforma.RecruitmentCycleID, event.ID),
 		Start: &calendar.EventDateTime{
-			DateTime: fmt.Sprintf("%s+05:30", time.UnixMilli(int64(event.StartTime)).In(loc).Format("2006-01-02T15:04")),
+			DateTime: time.UnixMilli(int64(event.StartTime)).In(loc).Format(time.RFC3339),
 			TimeZone: time_zone,
 		},
 		End: &calendar.EventDateTime{
-			DateTime: fmt.Sprintf("%s+05:30", time.UnixMilli(int64(event.StartTime)).In(loc).Format("2006-01-02T15:04")),
+			DateTime: time.UnixMilli(int64(event.EndTime)).In(loc).Format(time.RFC3339),
 			TimeZone: time_zone,
 		},
 	}
@@ -181,7 +181,7 @@ func putEventHandler(ctx *gin.Context) {
 		}
 	}
 
-	cevent, err = cal_srv.Events.Update(cID, event.CalID, cevent).Do()
+	_, err = cal_srv.Events.Update(cID, event.CalID, cevent).Do()
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
