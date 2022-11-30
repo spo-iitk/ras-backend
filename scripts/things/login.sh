dbd22611083ce08f247c24f055ac8680b5614b76
Harshit Raj <harshitr20@iitk.ac.in>
2022-11-30 16:34:43 +0530

    remove useless script

--- scripts/things/login.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-curl "https://placement.iitk.ac.in/api/auth/login" \
-  -H 'Content-Type: application/json' \
-  -X POST \
-  -d '{
-  "user_id": "",
-  "password": "",
-  "remember_me": true
-}'
