b2510c79d9d347eaa01c18fce302471d91fea27c
Abhishek Shree <abhi1234456@gmail.com>
2023-04-24 23:36:18 +0530

    log to stdout

--- config/logrus.go
+++ config/logrus.go
@@ -1,19 +1,17 @@
 package config
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/sirupsen/logrus"
 )
 
 func logrusConfig() {
 	logrus.SetLevel(logrus.DebugLevel)
 	logrus.SetReportCaller(true)
-	f, err := os.OpenFile("raslog.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
-	if err != nil {
-		fmt.Printf("error opening file: %v", err)
-		panic(err)
-	}
-	logrus.SetOutput(f)
+
+	// f, err := os.OpenFile("raslog.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	// if err != nil {
+	// 	fmt.Printf("error opening file: %v", err)
+	// 	panic(err)
+	// }
+	// logrus.SetOutput(f)
 }
