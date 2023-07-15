c116e27797464c3b29290f4f2578e5b7fbcaa952
Krishnansh5 <97296908+Krishnansh5@users.noreply.github.com>
2023-07-16 01:53:05 +0530

    Delete inter_service_calls directory

--- inter_service_calls/student.rc.go
+++
@@ -1,8 +0,0 @@
-package inter_service_calls
-
-// import "github.com/spo-iitk/ras-backend/rc"
-
-// func getStudentRegisteredRCs() {
-// 	var registered_rcs []rc.RecruitmentCycle
-// 	err := rc.FetchRCsByStudent()
-// }
