df501ec1efb039860f4d698c9fd57f42a176507a
Abhimanyu-dev <abhimanyusolanki4841@gmail.com>
2024-12-01 00:13:02 +0530

    Added Walk In

--- application/admin.student.go
+++ application/admin.student.go
@@ -136,6 +136,11 @@ func postStudentsByEventHandler(mail_channel chan mail.Mail) gin.HandlerFunc {
 			msg += " in the profile of " + proforma.Profile
 
 			mail_channel <- mail.GenerateMails(req.Emails, "Congratulations", msg)
+		} else if evnt.Name == string(WALKIN) {
+			msg := "Dear student" + "\n\n"
+			msg += "You have been selected for the walk in interview for the profile " + proforma.Profile + " by " + proforma.CompanyName + "."
+
+			mail_channel <- mail.GenerateMails(req.Emails, "Update on Application", msg)
 		} else {
 			msg := "Dear student" + "\n\n"
 			msg += "You have advanced to the stage of " + evnt.Name + " for the recruitment process of profile "
